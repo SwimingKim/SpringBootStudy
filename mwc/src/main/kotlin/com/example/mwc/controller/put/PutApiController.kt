dfdee9e0d77bd27235ac8cc6b1e17ade2a5c577b
skim <rumex13@naver.com>
2020-11-21 20:37:15 +0900

    valid

--- mwc/src/main/kotlin/com/example/mwc/controller/put/PutApiController.kt
+++ mwc/src/main/kotlin/com/example/mwc/controller/put/PutApiController.kt
@@ -3,7 +3,12 @@ package com.example.mwc.controller.put
 import com.example.mwc.model.http.Result
 import com.example.mwc.model.http.UserRequest
 import com.example.mwc.model.http.UserResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api")
@@ -20,39 +25,53 @@ class PutApiController {
     }
 
     @PutMapping(path = ["/put-mapping/object"])
-    fun putMappingObject(@RequestBody userRequest: UserRequest): UserResponse {
-        // 0. Response
-        return UserResponse().apply {
-            // 1. result
-            this.result = Result().apply {
-                this.resultCode = "OK"
-                this.resultMessage = "성공"
+    fun putMappingObject(@Valid @RequestBody userRequest: UserRequest, bindingResult: BindingResult): ResponseEntity<Any> {
+        if (bindingResult.hasErrors()) {
+            // 500 error
+            val msg = StringBuilder()
+            bindingResult.allErrors.forEach {
+                val field = it as FieldError
+                val message = field.defaultMessage
+                msg.append(field.field + " : " + message + "\n")
             }
-        }.apply {
-            // 2. description
-            this.description = "~~~~~~~~~"
-        }.apply {
-            // 3. user mutable list
-            val userList = mutableListOf<UserRequest>()
-            userList.add(userRequest)
 
-            userList.add(UserRequest().apply {
-                this.name = "a"
-                this.age = 10
-                this.email = "a@gmail.com"
-                this.address = "a address"
-                this.phoneNumber = "010-1111-aaaa"
-            })
-            userList.add(UserRequest().apply {
-                this.name = "b"
-                this.age = 20
-                this.email = "b@gmail.com"
-                this.address = "b address"
-                this.phoneNumber = "010-1111-bbbb"
-            })
-
-            this.userRequest = userList
+            return ResponseEntity.badRequest().body(msg.toString())
         }
+
+        return ResponseEntity.ok("")
+
+        // 0. Response
+//        return UserResponse().apply {
+//            // 1. result
+//            this.result = Result().apply {
+//                this.resultCode = "OK"
+//                this.resultMessage = "성공"
+//            }
+//        }.apply {
+//            // 2. description
+//            this.description = "~~~~~~~~~"
+//        }.apply {
+//            // 3. user mutable list
+//            val userList = mutableListOf<UserRequest>()
+//            userList.add(userRequest)
+//
+//            userList.add(UserRequest().apply {
+//                this.name = "a"
+//                this.age = 10
+//                this.email = "a@gmail.com"
+//                this.address = "a address"
+//                this.phoneNumber = "010-1111-aaaa"
+//            })
+//            userList.add(UserRequest().apply {
+//                this.name = "b"
+//                this.age = 20
+//                this.email = "b@gmail.com"
+//                this.address = "b address"
+//                this.phoneNumber = "010-1111-bbbb"
+//            })
+//
+//            this.userRequest = userList
+//        }
     }
 
 }
--- mwc/src/main/kotlin/com/example/mwc/model/http/UserRequest.kt
+++ mwc/src/main/kotlin/com/example/mwc/model/http/UserRequest.kt
@@ -2,14 +2,26 @@ package com.example.mwc.model.http
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import javax.validation.constraints.*
 
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
 data class UserRequest(
+
+        @field:NotEmpty
+        @field:Size(min = 2, max = 8)
         var name: String? = null,
+
+        @field:PositiveOrZero // 0 < 숫자를 검증 0도 포함 (양수)
         var age: Int? = null,
+
+        @field:Email // email 양식
         var email: String? = null,
+
+        @field:NotBlank // 공백을 검증증
         var address: String? = null,
+
 //        @JsonProperty("phone_number")
+        @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\f{4}\$")
         var phoneNumber: String? = null // phone_number
 
 )
