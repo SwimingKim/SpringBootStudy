dd42f5b972dbb64fd269a39d2655c7c80c8a26c1
skim <rumex13@naver.com>
2020-11-23 22:54:11 +0900

    valid

--- mwc/src/main/kotlin/com/example/mwc/model/http/UserRequest.kt
+++ mwc/src/main/kotlin/com/example/mwc/model/http/UserRequest.kt
@@ -1,7 +1,10 @@
 package com.example.mwc.model.http
 
+import com.example.mwc.annotation.StringFormatDateTime
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import javax.validation.constraints.*
 
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
@@ -22,6 +25,21 @@ data class UserRequest(
 
 //        @JsonProperty("phone_number")
         @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\f{4}\$")
-        var phoneNumber: String? = null // phone_number
+        var phoneNumber: String? = null, // phone_number
 
-)
+
+        @field:StringFormatDateTime(pattern = "yyyy-MM-dd HH:mm:ss", message = "패턴이 올바르지 않습니다.")
+        var createdAt: String? = null // yyyy-MM-dd HH:mm:ss    ex) 2020-10-02 13:00:00
+
+) {
+
+//    @AssertTrue(message = "생성일자의 패턴은 yyyy-MM-dd HH:mm:ss 여야 합니다")
+//    private fun isValidCreatedAt(): Boolean { // 정상 true, 비정상 false
+//        return try {
+//            LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+//            true
+//        } catch (e: Exception) {
+//            false
+//        }
+//    }
+}
